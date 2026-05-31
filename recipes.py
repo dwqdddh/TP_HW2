0ceef40dad131698fc86383c680695b772d5798f
Хан Диана <diaalekhan@edu.hse.ru>
2026-05-31 18:24:59 +0300

    Реализован класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -103,3 +103,15 @@ class ShoppingList:
         new = ShoppingList()
         new._items = self._items + other._items
         return new
+    
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients = None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+                
+    def scale(self, ratio:float):
+        return DietaryRecipe(self.title, self.diet_type, super().scale(ratio).ingredients)
+    
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
