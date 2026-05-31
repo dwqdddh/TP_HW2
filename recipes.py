bd998f6c7ef73bae2859b86979683b5e5a4e2d90
Хан Диана <diaalekhan@edu.hse.ru>
2026-05-31 17:30:17 +0300

    Реализован класс Recipe

--- recipes.py
+++ recipes.py
@@ -25,4 +25,43 @@ class Ingredient:
         if type(other) != Ingredient:
             return False
         return self.name==other.name and self.unit==other.unit
+
+
+class Recipe:
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        self.ingredients = []
+        if ingredients!=None:
+            for i in ingredients:
+                self.add_ingredient(i)
+                
+    def add_ingredient(self, ingredient: Ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if (type(ratio)==int or type(ratio)==float) and ratio>0:
+            return True
+        return False
+    
+    def scale(self, ratio:float):
+        if Recipe.is_valid_ratio(ratio)==False:
+            raise ValueError("Не положительный коэффицент:", ratio)
+        ratio_recipe = Recipe(self.title)
+        for i in self.ingredients:
+            ratio_recipe.add_ingredient(Ingredient(i.name, i.quantity*ratio, i.unit))
+        return ratio_recipe
+    
+    def __str__(self):
+        string = f"{self.title} (список ингредиентов):"
+        for i in self.ingredients:
+            string += f"\n{i}" 
+        return string 
+
+    def __len__(self):
+        return len(self.ingredients)
     
